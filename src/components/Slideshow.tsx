635888da0cacfc02d493634fd4736392c92752c0
nsaritzky <nsaritzky@gmail.com>
2023-12-27 10:39:38 -0800

    Slideshow fixes

--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -1,22 +1,19 @@
-import { For } from "solid-js"
 import { Autoplay, Pagination } from "swiper"
 import { Swiper, SwiperSlide } from "swiper/solid"
 
 import "swiper/css"
 
-import type { Component } from "solid-js"
+import type { ParentComponent } from "solid-js"
 import "swiper/css/pagination"
 
-interface Slide {
-  url: string
-  caption: string
-}
+type SlotKey = `slot-${number}`
 
 interface Props {
-  slides: Slide[]
+  count: number
+  [key: SlotKey]: any
 }
 
-const SlideShow: Component<Props> = (props) => {
+const SlideShow: ParentComponent<Props> = (props) => {
   return (
     <Swiper
       // install Swiper modules
@@ -24,31 +21,15 @@ const SlideShow: Component<Props> = (props) => {
       spaceBetween={50}
       slidesPerView={1}
       pagination={{ clickable: true }}
-      height={700}
       autoplay={{ delay: 5000 }}
     >
-      <For each={props.slides}>
-        {(slide) => (
-          <SwiperSlide>
-            <div class="h-min w-full bg-black">
-              <div class="relative mx-auto w-max">
-                <img
-                  class="mx-auto h-[600px] lg:h-[700px]"
-                  src={slide.url}
-                  alt={slide.caption}
-                />
-                <p
-                  class={`absolute bottom-0 w-full bg-black/50 px-2 pb-8 text-center text-white ${
-                    !slide.caption && "hidden"
-                  }`}
-                >
-                  {slide.caption}
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
-        )}
-      </For>
+      {[...Array(props.count).keys()].map((i) => (
+        <SwiperSlide>
+          <div class="h-min w-full bg-black">
+            <div class="relative mx-auto w-max">{props[`slot-${i}`]}</div>
+          </div>
+        </SwiperSlide>
+      ))}
     </Swiper>
   )
 }
