5fdc11dd59fe0fba255fb52e9c9a5e2548c0e498
nsaritzky <nsaritzky@gmail.com>
2024-01-22 05:12:17 -0800

    Add site and base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
-import { defineConfig } from "astro/config";
-import solidJs from "@astrojs/solid-js";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
+import { defineConfig } from "astro/config"
+import solidJs from "@astrojs/solid-js"
+import tailwind from "@astrojs/tailwind"
+import icon from "astro-icon"
 
-import mdx from "@astrojs/mdx";
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), tailwind(), icon(), mdx()],
+  site: "https://gocongress2024.github.io",
+  base: "/website",
   vite: {
     ssr: {
-      noExternal: ["swiper"]
-    }
-  }
-});
+      noExternal: ["swiper"],
+    },
+  },
+})
